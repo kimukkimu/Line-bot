7bd888678a6f4ed3e05c60d78f1b25cfef283640
KentaKimura <kentakimura@mbp.local>
2017-11-06 20:18:19 +0900

    edit settings.py

--- api/settings.py
+++ api/settings.py
@@ -120,3 +120,14 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'safe-crag-89614.herokuapp.com']
