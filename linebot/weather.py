fa9c3e44944eea84cddc24c703bb3372c1c58cd3
KentaKimura <kentakimura@mbp.local>
2017-11-07 13:58:05 +0900

    edit settings.py

--- linebot/weather.py
+++ linebot/weather.py
@@ -2,7 +2,7 @@ import requests
 
 
 def weather_response():
-    api_url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130040'
+    api_url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=260010'
     weather_date = requests.get(api_url).json()
 
     response = weather_date['title'] + '\n'
